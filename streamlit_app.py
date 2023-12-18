0e41d6617b2a2f2215ba90c9b0c69816b5593ea4
rkerlegand <154259715+rkerlegand@users.noreply.github.com>
2023-12-18 18:53:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,29 @@
 import streamlit as st
+from plot import ggscatter
+import pandas as pd
+import nfl_data_py as nfl
 
-st.write('Hello World!')
+#Import data
+
+pbp = nfl.import_pbp_data([2023])
+pbp = pd.DataFrame(pbp)
+
+# Define the list of teams (replace with actual team list)
+teams_list = pbp['posteam'].unique().tolist()
+
+st.title('NFL Team Data Comparison')
+
+# Team selectors
+team1 = st.selectbox('Select Team 1:', teams_list)
+team2 = st.selectbox('Select Team 2:', teams_list)
+
+# Button to generate plot
+if st.button('Compare Teams'):
+    # Filter the data for the selected teams
+    filtered_data = pbp[pbp['posteam'].isin([team1, team2])]
+    
+    # Generate the scatter plot
+    fig = ggscatter('air_yards', 'yards_after_catch', filtered_data, hue='posteam', reference='Fit')
+
+    # Display the plot
+    st.pyplot(fig)
